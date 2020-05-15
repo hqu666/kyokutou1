da4b36aaf82a8d0f13ac9600d453b167c33b87db
CORESOFT\hkuwayama <hkuwayama@coresoft-net.co.jp>
2020-05-15 14:01:28 +0900

    Log出し対応

---
+++ KGSample/KGSample/KGSample/CS_Util.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace KGSample {
+	class CS_Util {
+		/// <summary>
+		/// デバッグログ
+		/// 出荷時は　debugNow　をfalseに
+		/// </summary>
+		/// <param name="TAG"></param>
+		/// <param name="dbMsg"></param>
+		public void MyLog(String TAG, String dbMsg)
+		{
+			if (Constant.debugNow) {
+				Console.WriteLine(TAG + " : " + dbMsg);
+				//System.Diagnostics.Trace.WriteLine(TAG + " : " + dbMsg);
+				//System.Diagnostics.Debug.WriteLine(TAG + " : " + dbMsg);
+			}
+		}
+
+		public void MyErrorLog(String TAG, String dbMsg, Exception err)
+		{
+			if (Constant.errorCheckNow) {
+				//					Xamarin.Forms.Internals.Log.Warning(TAG, dbMsg);
+				Console.WriteLine(TAG + " : " + dbMsg + "でエラー発生;" + err);
+			}
+		}
+
+		///// <summary>
+		///// ダイアログ表示
+		///// </summary>
+		///// <param name="titolStr">タイトル</param>
+		///// <param name="msgStr">メッセージ</param>
+		///// 		/// http://furuya02.hatenablog.com/entry/20140426/1398477869
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="titolStr"></param>
+		/// <param name="msgStr"></param>
+		/// <param name="buttns">MessageBoxButtons.OKCancel</param>
+		/// <param name="icon">MessageBoxIcon.Exclamation</param>
+		/// <param name="defaultButton">MessageBoxDefaultButton.Button2</param>
+		/// <returns></returns>
+		//public DialogResult MessageShow(String titolStr, String msgStr, MessageBoxButtons buttns, MessageBoxIcon icon, MessageBoxDefaultButton defaultButton)
+		//{
+		//	String TAG = "MessageShow";
+		//	String dbMsg = "開始";
+		//	DialogResult result = 0;
+		//	try {
+		//		dbMsg = "titolStr=" + titolStr;
+		//		dbMsg += "mggStr=" + msgStr;
+
+		//		//メッセージボックスを表示する
+		//		result = MessageBox.Show(msgStr, titolStr, buttns, icon, defaultButton);
+		//		dbMsg += ",result=" + result;
+		//		MyLog(TAG, dbMsg);
+		//	} catch (Exception er) {
+		//		MyLog(TAG, dbMsg + "で" + er.ToString());
+		//	}
+		//	return result;
+		//}
+	}
+}
--- KGSample/KGSample/KGSample/GCalender.xaml.cs
+++ KGSample/KGSample/KGSample/GCalender.xaml.cs
@@ -20,70 +20,95 @@ namespace KGSample {
 	/// GCalender.xaml の相互作用ロジック
 	/// </summary>
 	public partial class GCalender : MetroWindow {
+		CS_Util Util = new CS_Util();
+
+		//LocalFileUtil LFUtil = new LocalFileUtil();
+		//GoogleAuthUtil GAuthUtil = new GoogleAuthUtil();
+		//GoogleCalendarUtil GCalendarUtil = new GoogleCalendarUtil();
+		//GoogleDriveUtil GDriveUtil = new GoogleDriveUtil();
 		private Rectangle selectedRec;
 
 		public GCalender()
 		{
-			InitializeComponent();
-
-			CalendarGropu1.Header = String.Format("{0:yyyy年MM月}", DateTime.Now);
-
-			// 当月の月初を取得
-			var firstDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
-
-			// 曜日番号の取得
-			int dayOfWeek = (int)firstDate.DayOfWeek;
-
-			// 月末を取得
-			int lastDay = firstDate.AddMonths(1).AddDays(-1).Day;
-
-			// 1日から月末までを走査
-			for (int day = 1; day <= lastDay; day++) {
-				// セル位置
-				int index = (day - 1) + dayOfWeek;
-				// 横位置
-				int x = index % 7;
-				// 縦位置
-				int y = index / 7;
-
-				// 土日は文字色を変更する
-				Color color = Colors.Black;
-				if (x == 0) {
-					color = Colors.Red;
-				} else if (x == 6) {
-					color = Colors.Blue;
-				}
-
-				// テキストブロックを生成してグリッドに追加
-				var tb = new TextBlock() {
-					Text = string.Format("{0}", day),
-					FontSize = 12,
-					Foreground = new SolidColorBrush(color),
-					Padding = new Thickness(0, 10, 10, 0),
-					HorizontalAlignment = HorizontalAlignment.Right,
-					VerticalAlignment = VerticalAlignment.Top
-				};
-				this.CalendarGrid.Children.Add(tb);
-				tb.SetValue(Grid.ColumnProperty, x);
-				tb.SetValue(Grid.RowProperty, y + 1);
-
-				// 四角形を生成してグリッドに追加
-				// セルの枠線などを表示し、イベントをハンドリングする用
-				var rec = new Rectangle();
-				rec.HorizontalAlignment = HorizontalAlignment.Stretch;
-				rec.VerticalAlignment = VerticalAlignment.Stretch;
-				// 背景色を設定しないとイベントを検知できないらしいので透過色を設定
-				rec.Fill = Brushes.Transparent;
-				// 枠線を調整
-				rec.Margin = (x == 6) ? new Thickness(0.0, -1.0, 0.0, 0.0) : new Thickness(0.0, -1.0, -1.0, 0.0);
-				// イベント設定
-				rec.MouseLeftButtonDown += date_MouseLeftButtonDown;
-				this.CalendarGrid.Children.Add(rec);
-				rec.SetValue(Grid.ColumnProperty, x);
-				rec.SetValue(Grid.RowProperty, y + 1);
-
+			string TAG = "GCalender";
+			string dbMsg = "[GCalender]";
+			try {
+				InitializeComponent();
+				DateTime taregetDate = DateTime.Now;
+				dbMsg += "今日は" + String.Format("{0:yyyy年MM月}", taregetDate);
+				Util.MyLog(TAG, dbMsg);
+				MakeCalender(taregetDate);
+			} catch (Exception er) {
+				Util.MyErrorLog(TAG, dbMsg, er);
 			}
 		}
+
+		public void MakeCalender(DateTime taregetDate)
+		{
+			string TAG = "MakeCalender";
+			string dbMsg = "[GCalender]";
+			try {
+				dbMsg += "対象年月" + String.Format("{0:yyyy年MM月}", taregetDate);
+
+				CalendarGropu1.Header = String.Format("{0:yyyy年MM月}", taregetDate);
+
+				// 当月の月初を取得
+				var firstDate = new DateTime(DateTime.Now.Year, taregetDate.Month, 1);
+				// 曜日番号の取得
+				int dayOfWeek = (int)firstDate.DayOfWeek;
+				// 月末を取得
+				int lastDay = firstDate.AddMonths(1).AddDays(-1).Day;
+				// 1日から月末までを走査
+				for (int day = 1; day <= lastDay; day++) {
+					// セル位置
+					int index = (day - 1) + dayOfWeek;
+					// 横位置
+					int x = index % 7;
+					// 縦位置
+					int y = index / 7;
+
+					// 土日は文字色を変更する
+					Color color = Colors.Black;
+					if (x == 0) {
+						color = Colors.Red;
+					} else if (x == 6) {
+						color = Colors.Blue;
+					}
+
+					// テキストブロックを生成してグリッドに追加
+					var tb = new TextBlock() {
+						Text = string.Format("{0}", day),
+						FontSize = 12,
+						Foreground = new SolidColorBrush(color),
+						Padding = new Thickness(0, 10, 10, 0),
+						HorizontalAlignment = HorizontalAlignment.Right,
+						VerticalAlignment = VerticalAlignment.Top
+					};
+					this.CalendarGrid.Children.Add(tb);
+					tb.SetValue(Grid.ColumnProperty, x);
+					tb.SetValue(Grid.RowProperty, y + 1);
+
+					// 四角形を生成してグリッドに追加
+					// セルの枠線などを表示し、イベントをハンドリングする用
+					var rec = new Rectangle();
+					rec.HorizontalAlignment = HorizontalAlignment.Stretch;
+					rec.VerticalAlignment = VerticalAlignment.Stretch;
+					// 背景色を設定しないとイベントを検知できないらしいので透過色を設定
+					rec.Fill = Brushes.Transparent;
+					// 枠線を調整
+					rec.Margin = (x == 6) ? new Thickness(0.0, -1.0, 0.0, 0.0) : new Thickness(0.0, -1.0, -1.0, 0.0);
+					// イベント設定
+					rec.MouseLeftButtonDown += date_MouseLeftButtonDown;
+					this.CalendarGrid.Children.Add(rec);
+					rec.SetValue(Grid.ColumnProperty, x);
+					rec.SetValue(Grid.RowProperty, y + 1);
+				}
+				Util.MyLog(TAG, dbMsg);
+			} catch (Exception er) {
+				Util.MyErrorLog(TAG, dbMsg, er);
+			}
+		}
+
 
 		/// <summary>
 		/// セル（日）をクリックした際のイベントハンドラ.
@@ -92,23 +117,36 @@ namespace KGSample {
 		/// <param name="e"></param>
 		void date_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
 		{
-			// 既に選択されたセルがある場合は初期化
-			if (selectedRec != null) {
-				selectedRec.StrokeDashArray = null;
-				selectedRec.StrokeThickness = 0;
+			string TAG = "date_MouseLeftButtonDown";
+			string dbMsg = "[GCalender]";
+			try {
+				// 既に選択されたセルがある場合は初期化
+				if (selectedRec != null) {
+					selectedRec.StrokeDashArray = null;
+					selectedRec.StrokeThickness = 0;
+				}
+
+				// 枠線に点線をセット
+				Rectangle rec = sender as Rectangle;
+				rec.Stroke = Brushes.Black;
+				DoubleCollection dbc = new DoubleCollection();
+				dbc.Add(1);
+				dbc.Add(1);
+				rec.StrokeDashArray = dbc;
+				rec.StrokeThickness = 1;
+
+				// 選択セルの保持
+				selectedRec = rec; Util.MyLog(TAG, dbMsg);
+				Util.MyLog(TAG, dbMsg);
+			} catch (Exception er) {
+				Util.MyErrorLog(TAG, dbMsg, er);
 			}
-
-			// 枠線に点線をセット
-			Rectangle rec = sender as Rectangle;
-			rec.Stroke = Brushes.Black;
-			DoubleCollection dbc = new DoubleCollection();
-			dbc.Add(1);
-			dbc.Add(1);
-			rec.StrokeDashArray = dbc;
-			rec.StrokeThickness = 1;
-
-			// 選択セルの保持
-			selectedRec = rec;
 		}
 	}
 }
+
+
+
+/*
+ 【WPF】自作カレンダー　その１（とりあえず当月を表示）https://www.doraxdora.com/blog/2017/10/15/post-2760/
+ */
