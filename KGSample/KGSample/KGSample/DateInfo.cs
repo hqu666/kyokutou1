8a632a626661984d756aa4b7ed51cc3d77e2ef55
CORESOFT\hkuwayama <hkuwayama@coresoft-net.co.jp>
2020-05-15 15:16:02 +0900

    カレンダーの動的生成

---
+++ KGSample/KGSample/KGSample/DateInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGSample {
+	/// <summary>
+	/// 日付情報
+	/// </summary>
+	public class DateInfo : MonthInfo {
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="yearMonth"></param>
+		/// <param name="day"></param>
+		public DateInfo(String yearMonth, String day) : base(yearMonth)
+		{
+			this.YearMonthDay = yearMonth + day;
+			this.Date = new DateTime(int.Parse(getYear()), int.Parse(getMonth()), int.Parse(day));
+		}
+
+		public DateTime Date { set; get; }
+
+		/// <summary>
+		/// 年月日
+		/// </summary>
+		public String YearMonthDay { set; get; }
+
+		/// <summary>
+		/// 年月日（YYYY年MM月DD日）
+		/// </summary>
+		public String getYearMonthDayWithKanji()
+		{
+			return String.Format("{0:yyyy年MM月dd日（ddd）}", Date);
+		}
+
+		/// <summary>
+		/// 日を返します.
+		/// </summary>
+		/// <returns></returns>
+		public String getDay()
+		{
+			if (String.IsNullOrEmpty(YearMonthDay)) return "";
+			return YearMonthDay.Substring(6, 2);
+		}
+	}
+}
--- KGSample/KGSample/KGSample/GCalender.xaml.cs
+++ KGSample/KGSample/KGSample/GCalender.xaml.cs
@@ -17,136 +17,175 @@ using MahApps.Metro.Controls;
 
 namespace KGSample {
 	/// <summary>
-	/// GCalender.xaml の相互作用ロジック
+	/// MainWindow.xaml の相互作用ロジック
 	/// </summary>
 	public partial class GCalender : MetroWindow {
-		CS_Util Util = new CS_Util();
-		GoogleAuthUtil GAuthUtil = new GoogleAuthUtil();
-		GoogleCalendarUtil GCalendarUtil = new GoogleCalendarUtil();
-		GoogleDriveUtil GDriveUtil = new GoogleDriveUtil();
-		LocalFileUtil LFUtil = new LocalFileUtil();
-
 		private Rectangle selectedRec;
+		private System.Windows.Style selectedRecStyle;
 
 		public GCalender()
 		{
-			string TAG = "GCalender";
-			string dbMsg = "[GCalender]";
-			try {
-				InitializeComponent();
-				DateTime taregetDate = DateTime.Now;
-				dbMsg += "今日は" + String.Format("{0:yyyy年MM月}", taregetDate);
-				Util.MyLog(TAG, dbMsg);
-				MakeCalender(taregetDate);
-			} catch (Exception er) {
-				Util.MyErrorLog(TAG, dbMsg, er);
+			InitializeComponent();
+
+			// 前月から6カ月分のリストを作成し、当月を選択させる
+			List<MonthInfo> list = new List<MonthInfo>();
+			DateTime now = DateTime.Now;
+			DateTime dt = now.AddMonths(-1);
+			for (int i = 0; i < 6; i++) {
+				list.Add(new MonthInfo(String.Format("{0:yyyyMM}", dt)));
+				dt = dt.AddMonths(1);
 			}
+			this.cbYearMonth.ItemsSource = list;
+			this.cbYearMonth.DisplayMemberPath = "YearMonthWithKanji";
+			this.cbYearMonth.SelectedIndex = 1;
+
+			createCalendar(this.cbYearMonth.SelectedItem as MonthInfo);
 		}
 
-		public void MakeCalender(DateTime taregetDate)
+		/// <summary>
+		/// 年月コンボの選択変更イベントハンドラー.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void cbYearMonth_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
-			string TAG = "MakeCalender";
-			string dbMsg = "[GCalender]";
-			try {
-				dbMsg += "対象年月" + String.Format("{0:yyyy年MM月}", taregetDate);
-
-				CalendarGropu1.Header = String.Format("{0:yyyy年MM月}", taregetDate);
-
-				// 当月の月初を取得
-				var firstDate = new DateTime(DateTime.Now.Year, taregetDate.Month, 1);
-				// 曜日番号の取得
-				int dayOfWeek = (int)firstDate.DayOfWeek;
-				// 月末を取得
-				int lastDay = firstDate.AddMonths(1).AddDays(-1).Day;
-				// 1日から月末までを走査
-				for (int day = 1; day <= lastDay; day++) {
-					// セル位置
-					int index = (day - 1) + dayOfWeek;
-					// 横位置
-					int x = index % 7;
-					// 縦位置
-					int y = index / 7;
-
-					// 土日は文字色を変更する
-					Color color = Colors.Black;
-					if (x == 0) {
-						color = Colors.Red;
-					} else if (x == 6) {
-						color = Colors.Blue;
-					}
-
-					// テキストブロックを生成してグリッドに追加
-					var tb = new TextBlock() {
-						Text = string.Format("{0}", day),
-						FontSize = 12,
-						Foreground = new SolidColorBrush(color),
-						Padding = new Thickness(0, 10, 10, 0),
-						HorizontalAlignment = HorizontalAlignment.Right,
-						VerticalAlignment = VerticalAlignment.Top
-					};
-					this.CalendarGrid.Children.Add(tb);
-					tb.SetValue(Grid.ColumnProperty, x);
-					tb.SetValue(Grid.RowProperty, y + 1);
-
-					// 四角形を生成してグリッドに追加
-					// セルの枠線などを表示し、イベントをハンドリングする用
-					var rec = new Rectangle();
-					rec.HorizontalAlignment = HorizontalAlignment.Stretch;
-					rec.VerticalAlignment = VerticalAlignment.Stretch;
-					// 背景色を設定しないとイベントを検知できないらしいので透過色を設定
-					rec.Fill = Brushes.Transparent;
-					// 枠線を調整
-					rec.Margin = (x == 6) ? new Thickness(0.0, -1.0, 0.0, 0.0) : new Thickness(0.0, -1.0, -1.0, 0.0);
-					// イベント設定
-					rec.MouseLeftButtonDown += date_MouseLeftButtonDown;
-					this.CalendarGrid.Children.Add(rec);
-					rec.SetValue(Grid.ColumnProperty, x);
-					rec.SetValue(Grid.RowProperty, y + 1);
-				}
-				Util.MyLog(TAG, dbMsg);
-			} catch (Exception er) {
-				Util.MyErrorLog(TAG, dbMsg, er);
-			}
+			createCalendar(cbYearMonth.SelectedItem as MonthInfo);
 		}
 
+		/// <summary>
+		/// 指定された年月のカレンダーを作成
+		/// </summary>
+		/// <param name="monthInfo"></param>
+		private void createCalendar(MonthInfo monthInfo)
+		{
+
+			CalendarContainer.Children.Clear();
+
+			// グループボックス
+			GroupBox calendarGroup1 = new GroupBox();
+			calendarGroup1.Header = monthInfo.YearMonthWithKanji;
+			calendarGroup1.Style = FindResource("gp-normal") as System.Windows.Style;
+
+			// グリッド
+			Grid calendarGrid1 = new Grid();
+			calendarGrid1.Style = FindResource("grid-calendar") as System.Windows.Style;
+			calendarGrid1.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+			calendarGrid1.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+			calendarGrid1.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+			calendarGrid1.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+			calendarGrid1.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+			calendarGrid1.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+			calendarGrid1.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+			calendarGrid1.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(20) });
+			calendarGrid1.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+			calendarGrid1.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+			calendarGrid1.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+			calendarGrid1.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+			calendarGrid1.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+
+			// グリッド枠線
+			Rectangle border = new Rectangle();
+			border.Style = FindResource("rec-border") as System.Windows.Style;
+			calendarGrid1.Children.Add(border);
+
+			for (int col = 0; col < 7; col++) {
+				Rectangle week = new Rectangle();
+				week.Style = (col == 6) ? FindResource("rec-week-sat") as System.Windows.Style : FindResource("rec-week") as System.Windows.Style;
+				week.SetValue(Grid.ColumnProperty, col);
+				calendarGrid1.Children.Add(week);
+
+				Label lbWeek = new Label();
+				lbWeek.Style = FindResource("lb-week") as System.Windows.Style;
+				lbWeek.Content = ("日月火水木金土").Substring(col, 1);
+				lbWeek.SetValue(Grid.ColumnProperty, col);
+				calendarGrid1.Children.Add(lbWeek);
+			}
+
+			// ヘッダ行下線
+			Rectangle underlining = new Rectangle();
+			underlining.Style = FindResource("rec-underlining") as System.Windows.Style;
+			calendarGrid1.Children.Add(underlining);
+
+			// グループボックスにグリッドを追加
+			calendarGroup1.Content = calendarGrid1;
+			CalendarContainer.Children.Add(calendarGroup1);
+
+			// 当月の月初を取得
+			var firstDate = new DateTime(int.Parse(monthInfo.getYear()), int.Parse(monthInfo.getMonth()), 1);
+
+			// 曜日番号の取得
+			int dayOfWeek = (int)firstDate.DayOfWeek;
+
+			// 月末を取得
+			int lastDay = firstDate.AddMonths(1).AddDays(-1).Day;
+
+			// 1日から月末までを走査
+			for (int day = 1; day <= lastDay; day++) {
+				// セル位置
+				int index = (day - 1) + dayOfWeek;
+				// 横位置
+				int x = index % 7;
+				// 縦位置
+				int y = index / 7;
+
+				// テキストブロックを生成してグリッドに追加
+				var tb = new TextBlock();
+				tb.Text = string.Format("{0}", day);
+				// 土日は文字色を変更する
+				if (x == 0) {
+					tb.Style = FindResource("txb-date-sun") as System.Windows.Style;
+				} else if (x == 6) {
+					tb.Style = FindResource("txb-date-sat") as System.Windows.Style;
+				} else {
+					tb.Style = FindResource("txb-date") as System.Windows.Style;
+
+				}
+				calendarGrid1.Children.Add(tb);
+				tb.SetValue(Grid.ColumnProperty, x);
+				tb.SetValue(Grid.RowProperty, y + 1);
+
+				// 四角形を生成してグリッドに追加
+				// セルの枠線などを表示し、イベントをハンドリングする用
+				var rec = new Rectangle();
+				DateInfo dt = new DateInfo(monthInfo.YearMonth, string.Format("{0:00}", day));
+				rec.DataContext = dt;
+				// 枠線を調整
+				rec.Style = (x == 6) ? FindResource("rec-date-sat") as System.Windows.Style : FindResource("rec-date") as System.Windows.Style;
+				// イベント設定
+				rec.MouseLeftButtonDown += date_MouseLeftButtonDown;
+				calendarGrid1.Children.Add(rec);
+				rec.SetValue(Grid.ColumnProperty, x);
+				rec.SetValue(Grid.RowProperty, y + 1);
+			}
+		}
 
 		/// <summary>
 		/// セル（日）をクリックした際のイベントハンドラ.
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		void date_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		private void date_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
 		{
-			string TAG = "date_MouseLeftButtonDown";
-			string dbMsg = "[GCalender]";
-			try {
-				// 既に選択されたセルがある場合は初期化
-				if (selectedRec != null) {
-					selectedRec.StrokeDashArray = null;
-					selectedRec.StrokeThickness = 0;
-				}
-
-				// 枠線に点線をセット
-				Rectangle rec = sender as Rectangle;
-				rec.Stroke = Brushes.Black;
-				DoubleCollection dbc = new DoubleCollection();
-				dbc.Add(1);
-				dbc.Add(1);
-				rec.StrokeDashArray = dbc;
-				rec.StrokeThickness = 1;
-
-				// 選択セルの保持
-				selectedRec = rec; Util.MyLog(TAG, dbMsg);
-				Util.MyLog(TAG, dbMsg);
-			} catch (Exception er) {
-				Util.MyErrorLog(TAG, dbMsg, er);
+			// 既に選択されたセルがある場合はスタイルを戻す
+			if (selectedRec != null) {
+				selectedRec.Style = selectedRecStyle;
 			}
+
+			// 選択されたセルの取得
+			Rectangle rec = sender as Rectangle;
+
+			// 選択セルの保持
+			selectedRec = rec;
+			selectedRecStyle = rec.Style;
+
+			// 選択時のスタイルに変更
+			rec.Style = FindResource("rec-date-selected") as System.Windows.Style;
+
+			// ラベルに日付をセット
+			lbSelectedDate.Content = (rec.DataContext as DateInfo).getYearMonthDayWithKanji();
 		}
 	}
 }
-
-
-
 /*
- 【WPF】自作カレンダー　その１（とりあえず当月を表示）https://www.doraxdora.com/blog/2017/10/15/post-2760/
+ 【WPF】自作カレンダー　その２（動的生成）		https://www.doraxdora.com/blog/2017/10/16/post-2779/
  */
