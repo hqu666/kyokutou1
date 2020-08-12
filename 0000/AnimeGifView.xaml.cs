3719beadfee308065e0c7457060d89b760747652
hiroomi kuwayama <hqu666@gmai.com>
2020-08-12 22:20:15 +0900

    t_events作成

---
+++ 0000/AnimeGifView.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows.Controls;
+using System;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Forms;
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace GoogleOSD.Controls {
+    /// <summary>
+	/// GIFアニメを再生できるView
+    /// </summary>
+    public partial class AnimeGifView : UserControl
+    {
+		private Bitmap _image = null;
+
+
+		public AnimeGifView()
+        {
+            InitializeComponent();
+        }
+
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			// フォームを表示するときの処理
+
+			// 画像ファイルをロードする
+			_image = new Bitmap("Resources/DarsBar.gif", true);
+
+			// pictureBox1の背景画像としてセット
+			pictureBox1.BackgroundImage = _image;
+			//サイズを合わせる　※はみ出すと動かない
+			pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+			// 描画(Paint)イベントハンドラを追加
+			pictureBox1.Paint += pictureBox1_Paint;
+			// アニメーション開始
+			ImageAnimator.Animate(_image, new EventHandler(Image_FrameChanged));
+
+		}
+
+		private void Image_FrameChanged(object o, EventArgs e)
+		{
+			// Paintイベントハンドラを呼び出す
+			pictureBox1.Invalidate();
+		}
+
+		void pictureBox1_Paint(object sender, PaintEventArgs e)
+		{
+			// イベントハンドラ：ピクチャボックスの描画
+			ImageAnimator.UpdateFrames(_image);
+		}
+
+		////////////////////////////////////////////////////
+		public static void MyLog(string TAG, string dbMsg)
+		{
+			CS_Util Util = new CS_Util();
+			Util.MyLog(TAG, dbMsg);
+		}
+
+		public static void MyErrorLog(string TAG, string dbMsg, Exception err)
+		{
+			CS_Util Util = new CS_Util();
+			Util.MyErrorLog(TAG, dbMsg, err);
+		}
+
+	}
+}
+
+//XMALでFormツールを使う	 https://gist.github.com/arosh/3213108
+//C#でアニメーションGIF画像を動かす	https://cammy.co.jp/technical/2016/09/10/c%E3%81%A7%E3%82%A2%E3%83%8B%E3%83%A1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3gif%E7%94%BB%E5%83%8F%E3%82%92%E5%8B%95%E3%81%8B%E3%81%99/
---
+++ 0000/ProgressDialog.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+
+
+namespace GoogleOSD {
+	/// <summary>
+	/// ProgressDialog.xaml の相互作用ロジック
+	/// </summary>
+	public partial class ProgressDialog : Window {
+
+		public ProgressDialog(string ProgressMsg = null)
+		{
+			string TAG = "ProgressDialog";
+			string dbMsg = "[ProgressDialog]";
+			try {
+				InitializeComponent();
+				MyLog(TAG, dbMsg);
+			} catch (Exception er) {
+				MyErrorLog(TAG, dbMsg, er);
+			}
+		}
+
+		public void SetMsg(string ProgressMsg)
+		{
+			string TAG = "SetMsg";
+			string dbMsg = "[ProgressDialog]";
+			try {
+				dbMsg += " ,ProgressMsg= " + ProgressMsg;
+	//			Progress_msg_tb.Content = ProgressMsg;
+				MyLog(TAG, dbMsg);
+			} catch (Exception er) {
+				MyErrorLog(TAG, dbMsg, er);
+			}
+		}
+
+
+		////////////////////////////////////////////////////
+		public static void MyLog(string TAG, string dbMsg)
+		{
+			CS_Util Util = new CS_Util();
+			Util.MyLog(TAG, dbMsg);
+		}
+
+		public static void MyErrorLog(string TAG, string dbMsg, Exception err)
+		{
+			CS_Util Util = new CS_Util();
+			Util.MyErrorLog(TAG, dbMsg, err);
+		}
+	}
+}
