beb9d1702ab9da8fd7ddaf27d380ee971e48da0c
hiroomi kuwayama <hqu666@gmai.com>
2020-08-17 00:03:18 +0900

    Projectのモデルまで

---
+++ googleOSD/googleOSD/googleOSD/Event_Registration.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GoogleOSD {
+	/// <summary>
+	/// Event_Registration.xaml の相互作用ロジック
+	/// </summary>
+	public partial class Event_Registration : Window {
+		public Event_Registration()
+		{
+			InitializeComponent();
+		}
+
+		private void Project_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
+
+		private void Cancel_bt_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Make_Event_bt_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
