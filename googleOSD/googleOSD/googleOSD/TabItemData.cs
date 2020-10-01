798fb77254188a4276d3d4569df7dc70f4247a8d
CORESOFT\hkuwayama <hkuwayama@coresoft-net.co.jp>
2020-10-01 11:37:11 +0900

    TabCon

---
+++ googleOSD/googleOSD/googleOSD/TabItemData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleOSD {
+	class TabItemData {
+		/// <summary>
+		/// ヘッダ
+		/// </summary>
+		public string Header { get; set; }
+
+		/// <summary>
+		/// Tab内のコンテンツ
+		/// </summary>
+		public string Content { get; set; }
+	}
+}
