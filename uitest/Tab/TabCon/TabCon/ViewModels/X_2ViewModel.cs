04d0c67529ebf1918c732a5e1b9f649c45d92f27
hkuwayama <hkuwauama@coresoft-net.co.jp>
2021-01-05 18:11:53 +0900

    環境復元

--- uitest/Tab/TabCon/TabCon/ViewModels/X_2ViewModel.cs
+++ uitest/Tab/TabCon/TabCon/ViewModels/X_2ViewModel.cs
@@ -1,35 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Media;
 using System.Data.SqlClient;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media.Imaging;
 using System.Reflection;
-using System.Windows.Shapes;
 using Google.Apis.Drive.v3.Data;
-using Infragistics.Controls.Schedules;
 using Livet;
 using Livet.Commands;
 using Livet.Messaging.Windows;
-using Livet.EventListeners;
 using Task = System.Threading.Tasks.Task;
-using Livet.Messaging;
 
 using MySql.Data.MySqlClient;
 using TabCon.Views;
 using TabCon.Models;
 using TabCon.Enums;
 
-namespace TabCon.ViewModels {
+namespace TabCon.ViewModels
+{
 	public class X_2ViewModel : ViewModel  {
 		public string titolStr = "【スケジュール登録】";
 		GoogleCalendarUtil GCalendarUtil = new GoogleCalendarUtil();
